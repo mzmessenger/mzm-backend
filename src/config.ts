f3625ba7ae41d89c05e00ce18885a5910269056b
kohta ito <kohta110@gmail.com>
2020-05-17 17:38:55 +0900

    fix validation

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const MESSAGE_LIMIT = 20
 
 export const USER_LIMIT = 20
 
-export const BANNED_CHARS_REGEXP_IN_ROOM_NAME = /\/|\\|\s/
+export const BANNED_CHARS_REGEXP_IN_ROOM_NAME = /^@|\/|\\|\s/
 
 // https://en.wikipedia.org/wiki/Whitespace_character
 
